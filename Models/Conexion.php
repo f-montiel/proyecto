4ccd09922b711ffc7dd22c237f7e28c577eaf8d6
f-montiel <montiel.facu@gmail.com>
2017-11-27 23:30:52 -0300

    Model/Conexion, Model/Estudiante

---
+++ Models/Conexion.php
@@ -0,0 +1,24 @@
+<?php namespace Models;
+
+class Conexion
+{
+	private $datos = array(
+		"host" => "localhost",
+		"user" => "root",
+		"pass" => "root",
+		"db" => "proyecto"
+	);
+	private $con;
+
+	private function __construct(){
+		$this->con = new \mysqli($this -> datos["host"], $this -> datos["user"], $this -> datos["pass"], $this -> datos["db"]);
+	}
+	public function consultaSimple($sql){
+		$this->con->query($sql); 
+	}
+	public function consultaRetorno($sql){
+		$datos = $this->con->query($Sql);
+		return $datos; 
+	}
+}
+  ?>
